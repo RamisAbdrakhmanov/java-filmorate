594b463afe1fc857ba05ded0ce6ba8d0931a5676
RamisAbdrakhmanov <ramis-abdr@yandex.ru>
2023-04-26 19:12:35 +0300

    12.2.1 добавил все кроме тестов и логирования по ревью

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -25,27 +25,47 @@ public class ReviewController {
     @GetMapping
     public List<Review> getReviews(@RequestParam(defaultValue = "0", required = false) int filmId,
                                    @RequestParam(defaultValue = "10", required = false) int count) {
-        return null;
+        return reviewService.getReviews(filmId, count);
     }
 
     @GetMapping("/{reviewId}")
-    public Review getReview(@PathVariable int reviewId){
+    public Review getReview(@PathVariable int reviewId) {
         return reviewService.getReview(reviewId);
     }
 
     @PostMapping
-    public Review addReview(@Valid @RequestBody Review review){
+    public Review addReview(@Valid @RequestBody Review review) {
 
         return reviewService.addReview(review);
     }
 
     @PutMapping
-    public Review changeReview(@Valid @RequestBody Review review){
+    public Review changeReview(@Valid @RequestBody Review review) {
         return reviewService.changeReview(review);
     }
+
     @DeleteMapping("/{reviewId}")
-    public void deleteReview(@PathVariable int reviewId){
+    public void deleteReview(@PathVariable int reviewId) {
         reviewService.deleteReview(reviewId);
     }
 
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable(name = "id") int reviewId, @PathVariable int userId) {
+        reviewService.addLike(reviewId, userId);
+    }
+
+    @PutMapping("/{id}/dislike/{userId}")
+    public void addDislike(@PathVariable(name = "id") int reviewId, @PathVariable int userId) {
+        reviewService.addDislike(reviewId, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLike(@PathVariable(name = "id") int reviewId, @PathVariable int userId) {
+        reviewService.deleteLike(reviewId, userId);
+    }
+
+    @DeleteMapping("/{id}/dislike/{userId}")
+    public void deleteDislike(@PathVariable(name = "id") int reviewId, @PathVariable int userId) {
+        reviewService.deleteDislike(reviewId, userId);
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/friend/FriendDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/friend/FriendDao.java
@@ -1,7 +1,10 @@
 package ru.yandex.practicum.filmorate.storage.dao.friend;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
 public interface FriendDao {
     void addFriend(int userId, int friendId);
 
