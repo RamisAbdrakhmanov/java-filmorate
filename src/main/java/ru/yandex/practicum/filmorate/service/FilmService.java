1835edb19d21610c257f343df4b7122c6b7620a2
darik666 <tattsar666@yandex.ru>
2023-04-26 13:36:00 -0700

    Added GET most popular films-

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exeption.notfound.FilmNotFoundException;
-import ru.yandex.practicum.filmorate.exeption.notfound.GenreNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.dao.film.FilmDao;
