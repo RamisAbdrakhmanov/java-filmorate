1e54ed6dfaef036c1c42a65e5d8b113d22d5b2d7
t1mmycash <timaknizh@gmail.com>
2023-04-27 18:51:18 +0300

    feat : добавил функционал связанный с режиссерами и поправил кодстайл

--- src/main/java/ru/yandex/practicum/filmorate/exeption/notfound/DirectorNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/notfound/DirectorNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exeption.notfound;
 
-public class DirectorNotFoundException extends NotFoundException{
+public class DirectorNotFoundException extends NotFoundException {
     public DirectorNotFoundException(String message) {
         super(message);
     }
